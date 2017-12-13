c62c136bc7b08d8a3b2ecff38953addf4750b0fc
Irena Neidlin <XsolarXwhisperX@gmail.com>
2017-12-13 02:15:48 +0200

    second block

--- js/script.js
+++ js/script.js
@@ -1,10 +1,1 @@
-var swiper = new Swiper('.swiper-container', {
-    navigation: {
-        nextEl: '.js-carousel-nav-left',
-        prevEl: '.js-carousel-nav-right',
-    },
-    speed: 300,
-    effect: 'slide',
-    loop: 'true',
-    slidesPerView: 4
-});
+$('.parallax-window').parallax({imageSrc: 'assets/img/top_bg.jpg'});
