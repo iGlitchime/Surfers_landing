d75beb07ee9837347fb21d1ed684b0dd122f5133
Irena Neidlin <XsolarXwhisperX@gmail.com>
2017-12-27 00:41:59 +0200

    team block

--- js/script.js
+++ js/script.js
@@ -1,1 +1,2 @@
-$('.parallax-window').parallax({imageSrc: 'assets/img/top_bg.jpg'});
+//$('.parallax-window').parallax({imageSrc: 'assets/img/top_bg.jpg'});
+//$('.parallax-window').parallax({imageSrc: 'assets/img/second-block-bg.png'});
