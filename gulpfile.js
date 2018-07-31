17c44affd0d27b186c433e797c33d0694c46e6f0
iglitchime <xsolarxwhisperx@gmail.com>
2018-07-31 13:48:46 +0300

    test

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,6 @@
 let gulp = require('gulp'),
   plumber = require('gulp-plumber'),
   sass = require('gulp-sass'),
-  //compass = require('gulp-compass'),
   clean = require('gulp-clean-css');
 
 
@@ -10,10 +9,6 @@ gulp.task('sass', () => {
   gulp.src('src/scss/styles.scss')
     .pipe(plumber())
     .pipe(sass())
-    //.pipe(compass({
-    //  sass: 'src/scss',
-    //  css: 'build/css'
-    //}))
     .pipe(clean())
     .pipe(gulp.dest('build/css'));
 });
